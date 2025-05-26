19785a67147dd0f899f9968634e97ec6c68161b6
Pras <79097664+masprast@users.noreply.github.com>
2025-05-26 08:35:22 +0700

    Delete blog directory

--- blog/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# from .models import Comments, Likes, Posts
-
-# Register your models here.
-# admin.site.register(Posts)
-# admin.site.register(Comments)
-# admin.site.register(Likes)
